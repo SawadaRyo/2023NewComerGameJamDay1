064452871baad5eb5a5a3540bd22d02b9fec0811
SR21C <rysawad@gmail.com>
2023-05-25 14:32:36 +0900

    [update]敵の挙動を更新、敵の生成システムを更新

--- UnityProject/Assets/Enemy/Scripts/Enemy.cs
+++ UnityProject/Assets/Enemy/Scripts/Enemy.cs
@@ -1,47 +1,113 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using System;
+using UniRx;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour,IEnemy
+public class Enemy : MonoBehaviour, IEnemy
 {
-    [SerializeField,Header("当たり判定")]
+    [SerializeField, Header("エネミーのスピード")]
+    float _speed = 5f;
+    [SerializeField, Header("エネミーのジャンプ力")]
+    float _jumpPower = 3f;
+    [SerializeField,Range(0.1f,1), Header("エネミーのジャンプのインターバル")]
+    float _jumpInterval = 0.5f;
+    [SerializeField, Header("当たり判定")]
     Collider2D _collider;
-    [SerializeField,Header("EnemyのImage")]
+    [SerializeField, Header("エネミーのImage")]
     Renderer _renderer;
+    [SerializeField, Header("")]
+    Rigidbody2D _rb;
 
-    public void Move()
+    [Tooltip("")]
+    bool _isActive = false;
+    [Tooltip("")]
+    bool _onGround = false;
+    [Tooltip("")]
+    Transform _playerTransform;
+
+    public bool IsActive => _isActive;
+
+    public void Instance(Transform playerTransForm)
     {
-
+        _playerTransform = playerTransForm;
+        _isActive = false;
+        _rb.isKinematic = true;
+        _collider.enabled = false;
+        _renderer.enabled = false;
+        Observable.EveryUpdate()
+            .Where(_ => _isActive)
+            .Subscribe(_ =>
+            {
+                Move(_speed,_playerTransform);
+            }).AddTo(this);
+        Observable.EveryUpdate()
+            .Where(_ => JumpJudge(_playerTransform))
+            .Subscribe(_ =>
+            {
+                Jump(_jumpPower, _jumpInterval);
+            }).AddTo(this);
     }
 
-    public void Instance()
+    public void Move(float speed, Transform playerTransform)
     {
+        float distansX = playerTransform.position.x - transform.position.x;
+        Debug.Log(distansX);
+        if (Mathf.Abs(distansX) > 0.1f)
+        {
+            Vector2 moveVec = new Vector2(distansX, _rb.velocity.y);
+            _rb.velocity = moveVec.normalized * speed;
+        }
+        else if (Mathf.Abs(distansX) <= 0.1f)
+        {
+            _rb.velocity = new Vector2(0f, _rb.velocity.y);
+        }
+    }
 
+    public async void Jump(float jumpPower, float interval)
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(interval));
+        _rb.AddForce(transform.up * _jumpPower, ForceMode2D.Impulse);
+        _onGround = false;
+    }
+
+    bool JumpJudge(Transform playerTransform)
+    {
+        float distansY = playerTransform.position.y - transform.position.y;
+        if (distansY >= 1f && _onGround) return true;
+        return false;
     }
 
     public void Create(Transform generatePos)
     {
         gameObject.transform.position = generatePos.position;
+        _isActive = true;
+        _rb.isKinematic = false;
         _collider.enabled = true;
         _renderer.enabled = true;
     }
 
     public void Death()
     {
+        _isActive = false;
+        _rb.isKinematic = true;
         _collider.enabled = false;
         _renderer.enabled = false;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision)
+        if (collision.tag == "Player")
         {
-            Death();
+            Hit();
+        }
+        else if(collision.tag == "Ground")
+        {
+            _onGround = true;
         }
     }
 
     public void Hit()
     {
-        throw new System.NotImplementedException();
+
     }
 }
--- UnityProject/Assets/Enemy/Scripts/IEnemy.cs
+++ UnityProject/Assets/Enemy/Scripts/IEnemy.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public interface IEnemy
 {
+    public bool IsActive { get; }
+
     /// <summary>
-    /// エネミーの動き
+    /// エネミーの移動
     /// </summary>
-    public void Move();
+    public void Move(float speed, Transform playerTransform);
+    /// <summary>
+    /// エネミーのジャンプ
+    /// </summary>
+    /// <param name="jumpPower"></param>
+    /// <param name="playerTransform"></param>
+    public void Jump(float jumpPower, float interval);
     /// <summary>
     /// エネミーのインスタンス時実行関数
     /// </summary>
-    public void Instance();
+    public void Instance(Transform playerTransform);
     /// <summary>
     /// エネミーの生成時実行関数
     /// </summary>
