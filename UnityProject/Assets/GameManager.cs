aeffcd61590f28c07d9d25aadca8bfacfcfde6ee
kudononoka <106291674+kudononoka@users.noreply.github.com>
2023-05-25 12:39:43 +0900

    [update]GameManager一応完了・あとは参照だけ

--- UnityProject/Assets/GameManager.cs
+++ UnityProject/Assets/GameManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField, Header("Timer")]float _timer = 30;
+    /// <summary>“G‚ð‰½‘Ì“|‚µ‚½‚©</summary>
+    public static int _score;
+    GameState _state = GameState.Game;
+    public GameState gameState { get { return _state; } set { _state = value; } }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,32 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _timer -= Time.deltaTime;
+        if (_timer <= 0)
+        {
+            _state = GameState.GameClear;
+        }
         
+        switch(_state)
+        {
+            case GameState.GameClear:
+                //“G“|‚µ”‚ðŽQÆ
+                //ƒV[ƒ“‘JˆÚ
+                break;
+            case GameState.GameOver:
+                //ƒV[ƒ“‘JˆÚ
+                break;
+        }
+    }
+    public void ScoreReset()
+    {
+        _score = 0;
+    }
+
+    public enum GameState
+    {
+        Game,
+        GameClear,
+        GameOver,
     }
 }
