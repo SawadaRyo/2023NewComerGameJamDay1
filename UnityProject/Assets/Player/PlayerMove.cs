767c5c72c7e1173084288724dab79b6ff35b40cd
Tejima Tsuyoshi <133856078+LifeLight4649@users.noreply.github.com>
2023-05-25 10:52:28 +0900

    PlayerMove作成

---
+++ UnityProject/Assets/Player/PlayerMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    //Rigidbody2D rb1 = Get
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
