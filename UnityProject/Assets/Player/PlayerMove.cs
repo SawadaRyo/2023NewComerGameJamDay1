04669b2c961e53ca70bdd7be944dc98e48ea7ecd
Tejima Tsuyoshi <133856078+LifeLight4649@users.noreply.github.com>
2023-05-25 11:55:40 +0900

    横移動完了

--- UnityProject/Assets/Player/PlayerMove.cs
+++ UnityProject/Assets/Player/PlayerMove.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMove : MonoBehaviour
 {
-    //Rigidbody2D rb1 = Get
+    Rigidbody2D rb;
+    [SerializeField] float _speed;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+
+       
 
         
     }
@@ -15,6 +20,8 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(h * _speed,0) ;
+
     }
 }
